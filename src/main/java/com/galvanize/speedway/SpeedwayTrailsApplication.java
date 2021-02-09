eef0b121a6daf4c35404df10931514f31df103ff
Anurag Vishnoi <annu.vishnoi@gmail.com>
2021-02-08 19:02:12 -0500

    intial setup

---
+++ src/main/java/com/galvanize/speedway/SpeedwayTrailsApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.speedway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedwayTrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedwayTrailsApplication.class, args);
+	}
+
+}
